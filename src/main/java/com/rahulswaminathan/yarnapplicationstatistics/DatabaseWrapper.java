7b68dcf092503e05ed0287a537dc1c119b4b2bea
xu-hong <xuhong713@gmail.com>
2015-03-20 00:16:39 -0400

    ....

--- src/main/java/com/rahulswaminathan/yarnapplicationstatistics/DatabaseWrapper.java
+++ src/main/java/com/rahulswaminathan/yarnapplicationstatistics/DatabaseWrapper.java
@@ -183,16 +183,37 @@ class CapacitySchedulerMetricsThread implements Runnable {
         for (int i = 0; i < schedulerQueues.length; i++) {
             Scheduler.queue q = schedulerQueues[i];
             String insertTableSQL = "INSERT INTO capacity_scheduler_metrics"
-                + "(experiment_id, time_elapsed , queue_id, capacity) VALUES"
-                + "(?,?,?,?)";
+                + "(experiment_id, time_elapsed, capacity, used_capacity, max_capacity, absolute_capacity,
+                    absolute_max_capacity, absolute_used_capacity, num_applications, queue_name, state,
+                    resource_used_memory, resource_used_vcores, num_active_applications, num_pending_applications,
+                    num_containers, max_applications, max_applications_per_user, max_active_applications,
+                    max_active_applications_per_user) VALUES"
+                + "(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
             PreparedStatement pstmt = null;
 
             try {
                 pstmt = connection.prepareStatement(insertTableSQL);
                 pstmt.setInt(1, experimentId);
                 pstmt.setLong(2, timeElapsed);
-                pstmt.setInt(3, i);
-                pstmt.setFloat(4, q.getCapacity());
+                pstmt.setFloat(3, q.getCapacity());
+                pstmt.setFloat(4, q.getUsedCapacity());
+                pstmt.setFloat(5, q.getMaxCapacity());
+                pstmt.setFloat(6, q.getAbsoluteCapacity());
+                pstmt.setFloat(7, q.getAbsoluteMaxCapacity());
+                pstmt.setFloat(8, q.getAbsoluteUsedCapacity());
+                pstmt.setInt(9, q.getNumApplications());
+                pstmt.setString(10, q.getQueueName());
+                pstmt.setString(11, q.getState());
+                pstmt.setInt(12, q.getResourceUsed().getMemory());
+                pstmt.setInt(13, q.getResourceUsed().getvCores());
+                pstmt.setInt(14, q.getNumActiveApplications());
+                pstmt.setInt(15, q.getNumPendingApplications());
+                pstmt.setInt(16, q.getNumContainers());
+                pstmt.setInt(17, q.getMaxApplications());
+                pstmt.setInt(18, q.getMaxApplicationsPerUser());
+                pstmt.setInt(19, q.getMaxActiveApplications());
+                pstmt.setInt(20, q.getMaxActiveApplicationsPerUser());
+
              
                 pstmt.execute();
             } catch (SQLException e) {
